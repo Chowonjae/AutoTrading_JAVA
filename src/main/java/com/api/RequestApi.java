e0d507bf707b793d19dc7a26db32619365c74f0a
Chowonjae <sunmoon1030@gmail.com>
2022-05-16 19:10:58 +0900

    22/05/16 (api) 잔여 연결 횟수 조회 api 추가 (거래)

--- src/main/java/com/api/RequestApi.java
+++ src/main/java/com/api/RequestApi.java
@@ -11,17 +11,35 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RequestApi {
     Integer HTTP_RESP_CODE_START = 200;
     Integer HTTP_RESP_CODE_END = 400;
 
-//    public void _parse_remaining_req(remaining_req){
-//        try{
-//
-//        }
-//    }
+    public static String[] _parse_remaining_req(String remaining_req){
+        String[] result = new String[3];
+        try{
+            Pattern p = Pattern.compile("group=([a-z]+); min=([0-9]+); sec=([0-9]+)");
+            Matcher m = p.matcher(remaining_req);
+
+            while (m.find()){
+                String k = m.group().replace(";", "");
+                String[] a = k.split(" ");
+                for (int i = 0; i < a.length; i++){
+                    result[i] = a[i].split("=")[1];
+                }
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getClass().getName());
+            Arrays.fill(result, null);
+        }
+        return result;
+    }
     public String _send_post_request(String url, String authenticationToken, HashMap<String, String> params){
         String result = "";
         HttpEntity entity = null;
@@ -67,4 +85,8 @@ public class RequestApi {
         return result;
     }
     // 남은 요청 횟수 조회해서 거래 일시 정지 밑 진행 추가
+    public static void main(String[] argb){
+        String[] a = _parse_remaining_req("Remaining-Req: group=order; min=59; sec=4");
+        System.out.println(Arrays.toString(a));
+    }
 }
